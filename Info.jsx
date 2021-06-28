3cf24cc204d034ee39f8985ed37a3fab2f572761
SRSowmya <86369044+SRSowmya@users.noreply.github.com>
2021-06-28 13:52:56 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div class="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        It were all wonderful sessions attending your classes .Thank you Shaurya
+        Sinha Sir for teaching us from the scratch{" "}
+      </p>
+    </div>
+  );
+}
+
+export default Info;
